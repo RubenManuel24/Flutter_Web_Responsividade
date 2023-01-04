7995780f133523a83b3b18c7d9f604707617a02a
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2023-01-04 17:41:12 +0100

    Utilizando LayoutBuilder

---
+++ lib/layout_builder_regras_de_layout.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+class LayoutBuilderRegrasDeLayout extends StatefulWidget {
+  const LayoutBuilderRegrasDeLayout({super.key});
+
+  @override
+  State<LayoutBuilderRegrasDeLayout> createState() => _LayoutBuilderRegrasDeLayoutState();
+}
+
+class _LayoutBuilderRegrasDeLayoutState extends State<LayoutBuilderRegrasDeLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Regras Layout"),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.amber,
+        child: LayoutBuilder(
+          builder: (context, constraints){
+            var tamanho = constraints.maxHeight;
+            //print("Tamanho do Container :$tamanho ");
+             
+             if(tamanho <= 600){
+                return Center(
+                  child: Text("Celular"),
+                  );
+             }
+             else if(tamanho > 600 || tamanho <= 960){
+                return Center(
+                  child: Text("Celular maior e Tablet"),
+                  );
+             }
+             
+             else{
+              return Center(
+                  child: Text("Telas maiores"),
+                  );
+             }
+            
+          }
+          ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web/layout_builder_regras_de_layout.dart';
 import 'package:flutter_web/responsiidade_media_query.dart';
 import 'package:flutter_web/responsividade_com_orientation_builder.dart';
 import 'package:flutter_web/responsividade_row_e_column_usando_expanded.dart';
@@ -6,7 +7,7 @@ import 'package:flutter_web/responsividade_row_e_column_usando_expanded.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: ResponsividadeComOrientationBuilder(),
+      home: LayoutBuilderRegrasDeLayout(),
       )
   );
 }
